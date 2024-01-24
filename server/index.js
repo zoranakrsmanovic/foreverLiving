343b720baba7f424bd94e4c5397675b57ef41997
Zorana Krsmanovic <zorana.krsmanovic@outlook.com>
2024-01-24 19:53:06 +0100

    project init

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import connectToDatabase from './db.js';
+import express from 'express';
+import cors from 'cors';
+
+//Routes
+import productRoutes from './routes/productRoutes.js';
+
+
+connectToDatabase();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/products', productRoutes);
+
+//localhost : 5000/api/products
+
+
+
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Api is running...');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server runs on the port ${port}`);
+});
---
+++ server/models/Product.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const productsSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    images:{
+        type: Array,
+        required: true,
+        default: [],
+    },
+    brand:{
+        type: String,
+        required: true,
+    },
+    category:{
+        type: String,
+        required: true,
+    },
+    reviews:{
+        type: Array,
+        required: true,
+        default: [],
+    },
+    rating:{
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    numberOfReviews:{
+        type: Number,
+        required: true,
+    },
+    price:{
+        type: Number,
+        required: true,
+    },
+    stock:{
+        type: Number,
+        required: true,
+    },
+    productIsNew:{
+        type: Boolean,
+        required: true,
+    },
+    stripeId:{
+        type: String,
+    },
+
+}, {timestamps:true});
+
+const Product  = mongoose.model('Product', productsSchema);
+
+export default Product;
